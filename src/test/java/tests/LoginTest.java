02672a17bdf906bcf7edcb547c72d8f7e8ab99e0
Manikanta <youremail@example.com>
2025-07-17 11:47:53 +0530

    Initial commit: Selenium login test with CI

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTest {
+	
+	WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    @Test
+    public void validLogin() {
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        WebElement message = driver.findElement(By.cssSelector(".flash.success"));
+        Assert.assertTrue(message.isDisplayed(), "Success message not displayed");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
